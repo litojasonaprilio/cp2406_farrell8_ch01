88d4e14b9652c77356fadc1e70f613e817d7d676
Jason <jasonaprilio.lito@my.jcu.edu.au>
2018-07-09 16:14:07 +0800

    Finish all debugging exercises.

--- DebuggingExercises/DebugOne1.java
+++ DebuggingExercises/DebugOne1.java
@@ -1,7 +1,6 @@
-public class DebugOne1
-
-   /* This program displays a greeting */
-   public static void main(String[] args)
-   {
-      Systemoutprintl("Hello).
-   }
+public class DebugOne1 {
+    /* This program displays a greeting */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
